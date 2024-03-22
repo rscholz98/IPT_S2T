c6324e461bfd8dd4f9b57d9d1d452bcbd6285fbc
rscholz <richardscholz1@gmx.de>
2024-03-22 16:34:37 +0100

    - Add

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ import tempfile
 from pydub import AudioSegment
 from st_audiorec import st_audiorec
 from transformers import pipeline
-from streamlit_extras.app_logo import add_logo
-import os
-import base64
-
 import requests
 
 API_TOKEN = "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"
 
-def convert_facebook(audio_path):
+def transcribe_facebook(audio_path):
     url = "https://api-inference.huggingface.co/models/facebook/wav2vec2-large-xlsr-53-german"
     headers = {"Authorization": f"Bearer {API_TOKEN}"}
     with open(audio_path, "rb") as f:
@@ -21,31 +17,28 @@ def convert_facebook(audio_path):
     json_response = response.json()
     return json_response['text']
 
-
-# Function to convert audio to WAV format
-def convert_audio_to_wav(audio_file):
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp_file:
-        sound = AudioSegment.from_file(audio_file)
-        sound = sound.set_channels(1).set_frame_rate(16000)
-        sound.export(tmp_file.name, format="wav")
-        return tmp_file.name
-
 def transcribe_whisper(audio_path):
     model = whisper.load_model("base")
     result = model.transcribe(audio_path, fp16=False)
     return result["text"]
 
-def correct_output(text):
-    fix_spelling = pipeline("text2text-generation",model="oliverguhr/spelling-correction-german-base")
-    return fix_spelling(text, max_length=256)
-
 st.set_page_config(layout="wide")
 
-st.sidebar.image("./assets/structure.png")
-gap_height = 300  
-st.sidebar.markdown(f"<div style='margin: {gap_height}px;'></div>", unsafe_allow_html=True)
-st.sidebar.image("./assets/logo.png")
-st.sidebar.write("")
+with st.sidebar:
+    st.sidebar.image("./assets/structure.png")
+
+    option = st.selectbox(
+    "Select a example Audio file",
+    ("Richard Example"),
+    index=None,
+    placeholder="Select contact method...",
+    )
+
+    st.write('You selected:', option)
+
+    gap_height = 200
+    st.sidebar.markdown(f"<div style='margin: {gap_height}px;'></div>", unsafe_allow_html=True)  
+    st.sidebar.image("./assets/logo.png")
 
 tab1, tab2, tab3 = st.tabs([":gear: Main", ":loud_sound: Recording", ":blue_book: Insights"])
 
@@ -76,20 +69,40 @@ with tab1:
         with col2:
             transcription_message_2 = st.empty()
             transcription_message_2.text("Transcribing with Wav2Vec 2.0 - German...")
-            transcription_wav2vec = convert_facebook(audio_path)
+            transcription_wav2vec = transcribe_facebook(audio_path)
             st.text_area("Wav2Vec 2.0 - German Transcription", value=transcription_wav2vec, height=200, key="wav2vec")
             transcription_message_2.empty()
 
-        corrected_whisper = correct_output(transcription_whisper)
-        corrected_wav2vec = correct_output(transcription_wav2vec)
+        def correct_output(text):
+            fix_spelling = pipeline("text2text-generation",model="oliverguhr/spelling-correction-german-base")
+            return fix_spelling(text, max_length=256)
+
+        
+        
 
         with col1:
-            st.text_area("Corrected Whisper Transcription", value=corrected_whisper, height=200, key="corr_whisper")
+            transcription_message_3 = st.empty()
+            transcription_message_3.text("Checking with spelling-correction-german-base ...")
+            corrected_whisper = correct_output(transcription_whisper)
+            st.text_area("Corrected Whisper Transcription", value=corrected_whisper[0]['generated_text'], height=200, key="corr_whisper")
+            transcription_message_3.empty()
         with col2:
-            st.text_area("Corrected Wav2Vec 2.0 - German", value=corrected_wav2vec, height=200, key="corr_wav2vec")
+            transcription_message_4 = st.empty()
+            transcription_message_4.text("Checking with spelling-correction-german-base ...")
+            corrected_wav2vec = correct_output(transcription_wav2vec)
+            st.text_area("Corrected Wav2Vec 2.0 - German", value=corrected_wav2vec[0]['generated_text'], height=200, key="corr_wav2vec")
+            transcription_message_4.empty()
 
 with tab2:
     recorded_audio = st_audiorec()
+
+    # Function to convert audio to WAV format
+    def convert_audio_to_wav(audio_file):
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp_file:
+            sound = AudioSegment.from_file(audio_file)
+            sound = sound.set_channels(1).set_frame_rate(16000)
+            sound.export(tmp_file.name, format="wav")
+            return tmp_file.name
 
     if recorded_audio is not None:
         # Assuming recorded_audio is a bytes-like object; adjust as necessary based on st_audiorec's output
