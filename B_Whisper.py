e6e99f6f1d7929a4654150581dfa10d703baafc2
rscholz <richardscholz1@gmx.de>
2024-03-22 09:38:48 +0100

    - Add ffmpeg locally

--- B_Whisper.py
+++ B_Whisper.py
@@ -1,8 +1,13 @@
 import whisper
 import pickle
+import ffmpeg
+import os
+
+ffmpeg_path = "C:/ffmpeg/bin"  
+os.environ['PATH'] += f':{os.path.dirname(ffmpeg_path)}'
 
 model = whisper.load_model("base")
 
-result = model.transcribe("output.wav")
+result = model.transcribe("output.wav", fp16=False)
 
 print(result["text"])
